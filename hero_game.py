102f872add1ef38bf9aead08e2bbaf88f6014b4c
NBESS <nicholasdbess@gmail.com>
2020-04-09 11:59:54 -0400

    fixes object calls

--- hero_game.py
+++ hero_game.py
@@ -9,16 +9,19 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 from lib.hero import Hero
 from lib.goblin import Goblin
 
-block.power
-def main():
-    block.health()
-    block.power()
-    goo.health() 
-    goo.power() 
+block = Hero('Blockade', 10, 5)
+goo = Goblin('Goo E.', 6, 2)
 
-    while goo.health() > 0 and block.health() > 0:
-        print("You have %d health and %d power." % (block.health(), block.power()))
-        print("The goblin has %d health and %d power." % (goo.health(), goo.power()))
+
+def main():
+    block.health
+    block.power
+    goo.health 
+    goo.power 
+
+    while goo.health > 0 and block.health > 0:
+        print("You have %d health and %d power." % (block.health, block.power))
+        print("The goblin has %d health and %d power." % (goo.health, goo.power))
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -28,9 +31,9 @@ def main():
         user_input = input()
         if user_input == "1":
             # Hero attacks goblin
-            goo.health() -= block.power()
-            print("You do %d damage to the goblin." % block.power())
-            if goo.health() <= 0:
+            goo.health -= block.power
+            print("You do %d damage to the goblin." % block.power)
+            if goo.health <= 0:
                 print("The goblin is dead.")
         elif user_input == "2":
             pass
@@ -40,11 +43,11 @@ def main():
         else:
             print("Invalid input %r" % user_input)
 
-        if goo.health() > 0:
+        if goo.health > 0:
             # Goblin attacks hero
-            block.health() -= goo.power()
-            print("The goblin does %d damage to you." % goo.power())
-            if block.health <= 0:
+            block.health -= goo.power
+            print("The goblin does %d damage to you." % goo.power)
+            if block.health() <= 0:
                 print("You are dead.")
 
 main()
