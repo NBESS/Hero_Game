7526af20152d29e08047996faa1ccc4990ac945d
NBESS <nicholasdbess@gmail.com>
2020-04-09 11:15:57 -0400

    fixes bug in __init__ mispelling causing class not to import

--- lib/goblin.py
+++ lib/goblin.py
@@ -1,6 +1,8 @@
 class Goblin():
-    def __inint__(self, name, health=6, power=2):
+    def __init__(self, name, health=6, power=2):
         
         self.name = name
         self.health = health
         self.power = power
+
+goo = Goblin('Goo E.', 6, 2)
